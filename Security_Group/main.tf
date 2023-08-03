82d6ea611ad6a1d06cf0dcfa95295c34f9370b58
adwordshin <136399595+adwordshin@users.noreply.github.com>
2023-08-03 12:55:45 +0900

    VPC

--- Security_Group/main.tf
+++ Security_Group/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  cloud {
+    organization = "IaC_Task"
+#    hostname = "app.terraform.io"
+#
+     workspaces {
+       name = "VPC"
+     }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
 module "security_group" {
   source = "github.com/adwordshin/terraform_module/module/Security_Group"
 
