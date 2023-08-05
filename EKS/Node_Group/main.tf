9d2d2fea7c9a03294c7c24c32cda182acfeb5ee9
adwordshin <136399595+adwordshin@users.noreply.github.com>
2023-08-05 10:01:57 +0900

    VPC

--- EKS/Node_Group/main.tf
+++ EKS/Node_Group/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  cloud {
+    organization = "IaC_Task"
+#    hostname = "app.terraform.io"
+#
+     workspaces {
+       name = "VPC"
+     }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
 module "node-group" {
   source = "github.com/adwordshin/terraform_module/module/EKS/Node_Group"
 
